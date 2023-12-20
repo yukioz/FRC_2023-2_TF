a82b12edba9e44401b386cf28e932315ac0fa96b
yukioz <yukio.link@gmail.com>
2023-12-20 03:00:26 -0300

    configuração parcial.

---
+++ backend/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secretpassword'
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('message')
+def handle_message(msg):
+    emit('message', msg, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app)
